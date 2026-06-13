4b787e5f0e7a21f788f888c10661761eb0648b1b
Mansi Bisht <bisht.mansi1201@gmail.com>
2026-06-13 14:01:54 +0530

    Time: 1 ms (77.18%) | Memory: 46.8 MB (23.10%) - LeetSync

---
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.java
@@ -0,0 +1,38 @@
+class Solution{
+
+    public int removeDuplicates(int[] nums){
+        // if array is empty return 0
+        if(nums.length==0) return 0;
+
+        
+        int i=0;
+
+        //start from second element
+        for(int j=1; j< nums.length; j++){
+
+            //if new unique element is found
+            if(nums[j]!= nums[i]){
+                //move unique position forward
+                i++;
+                //place new unique element
+                nums[i]=nums[j];
+            }
+        }
+        return i+1;
+    }
+}
+
+public class removeduplicates{
+    public static void main(String[] args){
+        int[] nums= {0,0,0,1,1,2,2,2,2,3,3,3,3,4};
+
+        Solution sol= new Solution();
+        int k= sol.removeDuplicates(nums);
+
+        System.out.println("Unique count=" + k);
+        System.out.print("array after removing duplicatess:");
+            for(int x= 0; x< k;x++){  
+                System.out.print(nums[x]+  " ");
+            }
+        }
+}
